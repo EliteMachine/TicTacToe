c748aa6e5dd304ac293f53014f80b240fce6a09f
chronicall <Entari@gmail.com>
2013-11-16 18:16:13 +0000

    Added test case for isWin(), function that checks for a victory in TicTacToe.

--- TicTacToe/src/test/java/is/EliteMachine/app/AppTest.java
+++ TicTacToe/src/test/java/is/EliteMachine/app/AppTest.java
@@ -1,38 +1,26 @@
 package is.EliteMachine.app;
 
+import static org.junit.Assert.assertEquals;
+
 import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public boolean isWin(){
+        App app = new App();
+        assertEquals(true, app.checkWin([1, 2, 3]));
+        assertEquals(true, app.checkWin([4, 5, 6]));
+        assertEquals(true, app.checkWin([7, 8, 9]));
+        assertEquals(true, app.checkWin([1, 4, 7]));
+        assertEquals(true, app.checkWin([2, 5, 8]));
+        assertEquals(true, app.checkWin([3, 6, 9]));
+        assertEquals(true, app.checkWin([1, 5, 9]));
+        assertEquals(true, app.checkWin([7, 5, 3]));
+        assertEquals(false, app.checkWin([1, 5, 3]));
+        assertEquals(false, app.checkWin([7, 2, 3]));
+        assertEquals(false, app.checkWin([7, 2, 6]));
+        assertEquals(false, app.checkWin([8, 1, 3]));
+        assertEquals(false, app.checkWin([7, 2, 5]));
     }
 }
