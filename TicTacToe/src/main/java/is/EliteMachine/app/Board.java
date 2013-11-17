171c5ae13846ff180d045a3f48fc3a5f8d3985ff
Valgeir <valgeirb@gmail.com>
2013-11-17 14:02:10 +0000

    Minor modifications to the Board class.

--- TicTacToe/src/main/java/is/EliteMachine/app/Board.java
+++ TicTacToe/src/main/java/is/EliteMachine/app/Board.java
@@ -10,18 +10,14 @@ package is.EliteMachine.app;
 */
 
 public class Board {
-	private static char blank;
 	private static char [][] board_array;
 
 	public Board(){
-		blank = ' ';
-		board_array = new char[3][3];
-		// Initialize the board.
-		for (int i = 0; i < board_array.length; i++) {
-			for (int j = 0; j < board_array.length; j++) {
-				board_array[i][j] = blank;
-			}
-		}
+		board_array = new char[][]{
+		{' ', ' ', ' '},
+		{' ', ' ', ' '},
+		{' ', ' ', ' '}
+	   };
 	}
 	// Print the board.
 	public static void print_board(char[][] board_array){
