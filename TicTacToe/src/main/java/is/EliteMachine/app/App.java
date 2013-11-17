8868d375f4b9d561e12ddf02e7185a0bec7a792a
Ívar <ivaroddsson@gmail.com>
2013-11-17 11:54:50 +0000

    Must commit to get to merge, this code is broken

--- TicTacToe/src/main/java/is/EliteMachine/app/App.java
+++ TicTacToe/src/main/java/is/EliteMachine/app/App.java
@@ -1,13 +1,32 @@
 package is.EliteMachine.app;
-
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+import java.util.ArrayList;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private char blank;
+	private char[][] gridA;
+	public App(){
+		blank = ' ';
+		gridA = new char[3][3];
+		for(int i = 0; i < gridA.length; i++){
+			for(int k = 0; k < gridA.length; k++){
+	    			gridA[i][k] = blank;
+			}
+    		}
+	} 
+
+    	public static ArrayList<Character> printGrid(char[][] gridA){ 
+		ArrayList<Character> myList = new ArrayList<Character>();
+		for(int i = 0; i < gridA.length; i++){
+	    		for(int k = 0; k < gridA.length; i++){
+	    			myList.add(gridA[i][k]);
+            		}
+		}
+		return(myList);
+	}
+
+    	public static void main( String[] args )
+    	{
+     		new App();
+    	}
 }
--- TicTacToe/src/test/java/is/EliteMachine/app/AppTest.java
+++ TicTacToe/src/test/java/is/EliteMachine/app/AppTest.java
@@ -1,38 +1,15 @@
 package is.EliteMachine.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.Assert;
+import java.util.ArrayList;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+   public void testGrid()
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+	App a = new App();
+	ArrayList<Character> p = new ArrayList<Character>();
+	p = a.printGrid();
+	Assert.assertEquals("         ", p);
     }
 }
