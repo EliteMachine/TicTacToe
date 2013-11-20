4dd6cda703e7a3a21b3be3fd9773374ebb0f1e55
Bergsteinn <beggi@i-cd69322b.greenqloud.internal>
2013-11-20 10:56:39 +0000

    PlayerTest.java class that test the player class in the game

---
+++ TicTacToe/src/test/java/is/EliteMachine/app/PlayerTest.java
@@ -0,0 +1,20 @@
+package is.EliteMachine.app
+//This is a test class for the Player class in main
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PlayerTest
+{
+	@Test
+	public void isPlayerOneX(){
+		Player player = new Player();
+		
+		
+	}
+
+
+
+
+}
