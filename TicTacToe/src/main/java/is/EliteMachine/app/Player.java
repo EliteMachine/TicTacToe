82744bc8720bec0f6ac0b0205c6d99808e1a87a9
Ívar <ivaroddsson@gmail.com>
2013-11-20 12:23:03 +0000

    Added a player class

--- TicTacToe/src/main/java/is/EliteMachine/app/Player.java
+++ TicTacToe/src/main/java/is/EliteMachine/app/Player.java
@@ -1,5 +1,20 @@
 package is.EliteMachine.app;
 //This is a player class
 public class Player {
-	
+	private int playerTurn;
+	private char playerSymbol;
+
+	public Player(){
+		this.playerTurn = 0;
+		this.playerSymbol = ' ';
+	}
+
+	public static void XOPlayers(int playerTurn, char playerSymbol){
+		if(playerTurn % 2 == 0){
+			playerSymbol = 'X';
+		}
+		else{
+			playerSymbol = 'O';
+		}
+	}
 }
