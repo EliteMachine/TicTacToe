37311c84f8dbb5ea60eb92ce27c5ab0a85673b04
chronicall <Entari@gmail.com>
2013-11-22 20:38:55 +0000

    Added test for isSet function of Board.java, which checks if the requested field is set or not.

--- src/test/java/is/EliteMachine/app/BoardTest.java
+++ src/test/java/is/EliteMachine/app/BoardTest.java
@@ -5,14 +5,27 @@ import org.junit.Test;
 import java.util.Arrays;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 public class BoardTest{
 
+    Board a = new Board();
 	Board b = new Board();
 	Board c = new Board();
+
 	@Test
-	public void player_mark_test(){
-		b.player_mark('X', 1, 1);
-		c.player_mark('X', 1, 0);
+	public void playerMarkTest(){
+		b.playerMark('X', 1, 1);
+		c.playerMark('X', 1, 0);
 		assertThat(b, is(not(c)));
 	}
+
+    @Test
+    public void checkFieldTest() {
+        a.playerMark('X', 1, 1);
+        assertEquals(true, isSet(1, 1));
+        assertEquals(false, isSet(0, 0));
+        assertEquals(false, isSet(2, 2));
+        a.playerMark('X', 0, 2);
+        assertEquals(true, isSet(0, 2));
+    }
 }
