d1a5cab24179345d78b389943101a219a85cbf4f
chronicall <Entari@gmail.com>
2013-11-23 11:29:17 +0000

    Edited Player.java, removed most of the things there as they were unecessary. Player class now only holds the player symbol, could add more to it later. Passes the tests for PlayerTest.java

--- src/main/java/is/EliteMachine/app/Player.java
+++ src/main/java/is/EliteMachine/app/Player.java
@@ -1,37 +1,14 @@
 package is.EliteMachine.app;
 //This is a player class
 public class Player {
-      private static int playerTurn;
       private static char playerSymbol;
 
-      public Player(){
-              playerTurn = 0;
-              playerSymbol = ' ';
-      }
-
-      public void XOPlayers(int playerTurn){
-              if(playerTurn % 2 == 0){
-                      playerSymbol = 'X';
-              }
-              else{
-                      playerSymbol = 'O';
-              }
-      }
-
-      public int getPlayerTurn(){
-              return playerTurn;
-      }
-
-      public void setPlayerSymbol(char player_Symbol){
-    	  playerSymbol = player_Symbol;
+      public Player(char playerSymbol){
+              this.playerSymbol = playerSymbol;
       }
 
       public char getPlayerSymbol(){
-              return playerSymbol;
-      }
-
-      public void incrementTurn(){
-              playerTurn += 1;
+          return this.playerSymbol;
       }
 }
 
